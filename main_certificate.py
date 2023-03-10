668ff3924271b8e6c3603dab41435aa28c29545d
Sabyasachi-Seal <yoboy907@gmail.com>
2023-03-10 23:14:05 +0530

    Added New Functions

--- main_certificate.py
+++ main_certificate.py
@@ -3,11 +3,14 @@ import os
 
 os.system("pip install -r requirements.txt")
 
+import csv
 from certificate import *
 from docx import Document
-import csv
 from docx2pdf import convert
+from openpyxl import Workbook, load_workbook
 
+mailerpath = "Data/Mail.xlsm"
+htmltemplatepath = "Data/mailtemplate.html"
 
 # create output folder if not exist
 try:
@@ -17,6 +20,29 @@ except OSError:
     pass
 
 
+def updatemailer(row, workbook, sheet, email, filepath, sub, body, status, cc=""):
+    sheet.cell(row=row, column=1).value = email
+    sheet.cell(row=row, column=2).value = cc
+    sheet.cell(row=row, column=3).value = sub
+    sheet.cell(row=row, column=4).value = body
+    sheet.cell(row=row, column=5).value = filepath
+    sheet.cell(row=row, column=6).value = status
+    workbook.save(filename = mailerpath)
+
+def getworkbook(filename):
+    wb = load_workbook(filename=filename, read_only=False, keep_vba=True)
+    sheet = wb.active
+    return wb, sheet
+
+def gethtmltemplate(htmltemplatepath=htmltemplatepath):
+    return open(htmltemplatepath, "r").read()
+
+def getmail(name, event, ambassador):
+    sub = f"[{event}] Certificate of Completion"
+    html = gethtmltemplate(htmltemplatepath)
+    body = html.format(name=name, event=event, ambassador=ambassador)
+    return sub, body
+
 def get_participants(f):
     data = [] # create empty list
     with open(f, mode="r", encoding='utf-8') as file:
@@ -25,27 +51,37 @@ def get_participants(f):
             data.append(row) # append all results
     return data
 
-def create_docx_files(filename, list_participate, ambassador):
+def create_docx_files(filename, list_participate):
 
-    for participate in list_participate:
+    wb, sheet = getworkbook(mailerpath)
+
+    event = input("Enter the event name: ")
+    ambassador = input("Enter Ambassador Name: ")
+
+    for index, participate in enumerate(list_participate):
         # use original file everytime
         doc = Document(filename)
 
-        name = participate["Name Surname"]
-        event = participate["Workshop"]
+        name = participate["Student Name"]
+        email = participate["Student Email"]
 
         replace_participant_name(doc, name)
         replace_event_name(doc, event)
         replace_ambassador_name(doc, ambassador)
+
+        doc.save('Output/Doc/{}.docx'.format(name))
+
         doc.save('Output/Doc/{}.docx'.format(name))
 
         # ! if your program working slowly, comment this two line and open other 2 line.
         print("Output/{}.pdf Creating".format(name))
         convert('Output/Doc/{}.docx'.format(name), 'Output/Pdf/{}.pdf'.format(name))
 
-        # ! Open those lines and comment above 2 lines if your program working extremely slow
-        # os.system("docx2pdf Output/Doc/")
-        # os.system("move Output\Doc\*pdf Output\PDF")
+        filepath = os.path.abspath('Output/Pdf/{}.pdf'.format(name))
+
+        sub, body = getmail(name, event, ambassador)
+
+        updatemailer(row=index, workbook=wb,  sheet=sheet, email=email, filepath=filepath, sub=sub, body=body, status="Send")
 
     
 # get certificate temple path
@@ -53,14 +89,11 @@ certificate_file = "Data/Event Certificate Template.docx"
 # get participants path
 participate_file = "Data/"+("Participant List.csv" if (input("Test Mode (Y/N): ").lower())[0]=="n" else "temp.csv")
 
-# Enter your name here [Ambassador Name]
-ambassador_name = input("Enter your name(This will be the host name on the certificates): ")
-
 # get participants
 list_participate = get_participants(participate_file);
 
 # process data
-create_docx_files(certificate_file, list_participate, ambassador_name)
+create_docx_files(certificate_file, list_participate)
 
 
 

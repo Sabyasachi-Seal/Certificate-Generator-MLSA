fd64271be462d4f5b966e47bb0402055867c87a9
Sabyasachi <yoboy907@gmail.com>
2023-12-19 14:19:39 +0530

    Further Fixes

--- main_certificate.py
+++ main_certificate.py
@@ -193,6 +193,7 @@ async def generate_certificates(
 
     os.system("rm -rf Output/Doc/*")
     os.system("rm -rf Output/PDF/*")
+    os.system("rm -rf static/certificates.zip")
 
     return StreamingResponse(get_data_from_file(), media_type="application/zip")
 

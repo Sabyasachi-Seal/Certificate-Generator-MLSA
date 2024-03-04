9b7f5a433d45e79f14e8a6e45108726fba8f6f23
Sabyasachi <yoboy907@gmail.com>
2024-03-04 14:05:23 +0530

    Minor Fix

--- main_certificate.py
+++ main_certificate.py
@@ -8,13 +8,17 @@ import aiofiles
 from docx import Document
 from typing import AsyncGenerator
 from openpyxl import load_workbook
-from fastapi.responses import HTMLResponse 
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Form, File, UploadFile, Request
 from fastapi.responses import FileResponse
-from certificate import replace_participant_name, replace_event_name, replace_ambassador_name
+from certificate import (
+    replace_participant_name,
+    replace_event_name,
+    replace_ambassador_name,
+)
 
 app = FastAPI()
 
@@ -44,9 +48,11 @@ try:
 except OSError:
     pass
 
+
 async def get_data_from_file() -> AsyncGenerator[bytes, None]:
     with open(file=zip_filename, mode="rb") as file_like:
         yield file_like.read()
+
 
 def clear_mailer_file(wb, sheet):
 
@@ -56,6 +62,7 @@ def clear_mailer_file(wb, sheet):
             cell.value = None
 
     wb.save(filename=mailerpath)
+
 
 def updatemailer(row, workbook, sheet, email, filepath, sub, body, status, cc=""):
     sheet.cell(row=row, column=1).value = email
@@ -67,19 +74,23 @@ def updatemailer(row, workbook, sheet, email, filepath, sub, body, status, cc=""
 
     workbook.save(filename=mailerpath)
 
+
 def getworkbook(filename):
     wb = load_workbook(filename=filename, read_only=False, keep_vba=True)
     sheet = wb.active
     return wb, sheet
 
+
 def gethtmltemplate(htmltemplatepath=htmltemplatepath):
     return open(htmltemplatepath, "r").read()
+
 
 def getmail(name, event, ambassador):
     sub = f"[MLSA] Certificate of Participation for {name}"
     html = gethtmltemplate(htmltemplatepath)
     body = html.format(name=name, event=event, ambassador=ambassador)
     return sub, body
+
 
 async def process_csv(csv_file):
 
@@ -88,35 +99,41 @@ async def process_csv(csv_file):
     first = True
 
     with open(csv_file) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        csv_reader = csv.reader(csv_file, delimiter=",")
+
+        name_index = 0
+        email_index = 0
+
         for line in csv_reader:
             if first:
                 first = False
+                for index, column in enumerate(line):
+                    if re.search(r"\bName\b", column):
+                        name_index = index
+                    elif re.search(r"\bEmail\b", column):
+                        email_index = index
                 continue
-            name, email = line
+            name = line[name_index]
+            email = line[email_index]
             participant_list.append({"Name": name.strip(), "Email": email.strip()})
-
-    # print(participant_list)
 
     return participant_list
 
+
 def convert_to_pdf(input_path, output_path):
-    cmd = [
-        "unoconv", 
-        "-f", "pdf", 
-        "-o", output_path, 
-        input_path
-    ]
+    cmd = ["unoconv", "-f", "pdf", "-o", output_path, input_path]
     process = subprocess.Popen(cmd, stderr=subprocess.PIPE)
     output, error = process.communicate()
     # print(output, error)
-    return output, error
+    return False, error
+
 
 def zip_folder(folder_path, zip_filename, additional_files):
 
     # print(os.listdir(folder_path))
 
-    with zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    with zipfile.ZipFile(zip_filename, "w", zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(folder_path):
             for file in files:
                 file_path = os.path.join(root, file)
@@ -129,6 +146,7 @@ def zip_folder(folder_path, zip_filename, additional_files):
                 arcname = os.path.relpath(file_path, os.path.dirname(file_path))
                 zipf.write(file_path, arcname)
 
+
 async def create_docx_files(filename, list_participate, event, ambassador):
 
     wb, sheet = getworkbook(mailerpath)
@@ -139,14 +157,13 @@ async def create_docx_files(filename, list_participate, event, ambassador):
     os.system("rm -rf Output/PDF/*")
 
     for index, participate in enumerate(list_participate):
-        
-        participate['Name'] = participate.pop(next(key for key in participate if re.search(r'\bName\b', key)))
-        participate['Email'] = participate.pop(next(key for key in participate if re.search(r'\bEmail\b', key)))
-        # use original file everytime
+
+        print(participate)
+
         name = participate["Name"]
         email = participate["Email"]
 
-        if email == '' or name == '':
+        if email == "" or name == "":
             continue
 
         doc = Document(filename)
@@ -155,25 +172,32 @@ async def create_docx_files(filename, list_participate, event, ambassador):
         replace_event_name(doc, event)
         replace_ambassador_name(doc, ambassador)
 
-        doc.save('./Output/Doc/{}.docx'.format(name))
+        doc.save("./Output/Doc/{}.docx".format(name))
 
-        # print(os.listdir("./Output/Doc/"))
+        convert_to_pdf(
+            "./Output/Doc/{}.docx".format(name), "./Output/PDF/{}.pdf".format(name)
+        )
 
-        convert_to_pdf('./Output/Doc/{}.docx'.format(name), './Output/PDF/{}.pdf'.format(name))
-
-        # print(os.listdir("./Output/PDF/"))
-
-        # print("Output/{}.pdf Creating".format(name))
-
-        filepath = os.path.abspath('./Output/PDF/{}.pdf'.format(name))
+        filepath = os.path.abspath("./Output/PDF/{}.pdf".format(name))
 
         sub, body = getmail(name, event, ambassador)
 
-        updatemailer(row=index+2, workbook=wb,  sheet=sheet, email=email, filepath=filepath, sub=sub, body=body, status="Send")
+        updatemailer(
+            row=index + 2,
+            workbook=wb,
+            sheet=sheet,
+            email=email,
+            filepath=filepath,
+            sub=sub,
+            body=body,
+            status="Send",
+        )
+
 
 @app.get("/", response_class=HTMLResponse)
 def read_item(request: Request):
     return templates.TemplateResponse("index.html", context={"request": request})
+
 
 @app.get("/{filepath}")
 def get_file(filepath: str):
@@ -181,9 +205,11 @@ def get_file(filepath: str):
     # print(file_path)
     return FileResponse(file_path)
 
+
 async def get_statinfo():
     with open(zip_filename, "rb") as file:
         yield file.read()
+
 
 def is_valid_csv(file_content: str) -> bool:
     try:
@@ -193,11 +219,12 @@ def is_valid_csv(file_content: str) -> bool:
     except csv.Error:
         return False
 
+
 @app.post("/generate_certificates")
 async def generate_certificates(
     event_name: str = Form(...),
     ambassador_name: str = Form(...),
-    participant_file: UploadFile = File(...)
+    participant_file: UploadFile = File(...),
 ):
 
     async with aiofiles.open(csv_file_path, "wb") as buffer:
@@ -208,23 +235,28 @@ async def generate_certificates(
         # Check if the content is a valid CSV
         if not is_valid_csv(content.decode()):
             return {"status_code": 400, "message": "Invalid CSV file"}
-    
+
         await buffer.write(content)
 
     # get certificate temple path
     certificate_file = "./Data/Event_Certificate_Template.docx"
 
     # get participants
-    list_participate = await process_csv(csv_file_path);
-    
-    await create_docx_files(certificate_file, list_participate, event=event_name, ambassador=ambassador_name)
+    list_participate = await process_csv(csv_file_path)
+
+    await create_docx_files(
+        certificate_file, list_participate, event=event_name, ambassador=ambassador_name
+    )
 
     additional_files = [mailerpath]
 
     zip_folder("./Output/PDF", zip_filename, additional_files)
 
-    return FileResponse(zip_filename, media_type="application/octet-stream", filename="certificates.zip")
+    return FileResponse(
+        zip_filename, media_type="application/octet-stream", filename="certificates.zip"
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Run the app with Uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
